0b686e8363fbb2a5da9a9844d1060ce37b33b765
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-06-11 21:57:01 -0300

    alteração ma estrutura de dados

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -2,7 +2,7 @@ import { User } from '@prisma/client';
 
 export class UserEntity implements User {
   updatedAt: Date;
-  id: string;
+  id: number;
   name: string;
   password: string;
   email: string;
